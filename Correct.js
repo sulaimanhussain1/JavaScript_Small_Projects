5ac38f570b0e06a3c060fe7ab116e17affb6aaad
Sulaiman Hussain <67717623+sulaimanhussain1@users.noreply.github.com>
2023-03-28 16:22:04 +0500

    Javascript Assignment.

---
+++ Correct.js
@@ -0,0 +1,16 @@
+
+
+// Problem no 5
+
+console.log("Enter a Correct Number! . Guess the number ? ");
+
+let cn = 26;
+
+let i; 
+
+while( i != cn){
+    console.log("Try Again");
+    i = prompt("Enter a Number : " );
+}
+
+console.log("You Win! : You have entered a correct number. ");
