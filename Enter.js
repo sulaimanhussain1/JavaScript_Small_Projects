ad075aa7b38c3573c71203105af2fd0f4e1847db
Sulaiman Hussain <67717623+sulaimanhussain1@users.noreply.github.com>
2023-04-02 15:50:48 +0500

    Javascript Assignment.

---
+++ Enter.js
@@ -0,0 +1,15 @@
+
+
+// problem no 5 
+
+// Create an array of numbers and take input from the user to add number in the array 
+
+let arr = [1,2 ,3 ,4, 5, 6, 7, 8,];
+console.log(arr);
+
+let a = prompt("Enter a number ? ");
+a = Number.parseInt(a);
+
+let b = arr.push(a);
+console.log(b);
+console.log(arr)
